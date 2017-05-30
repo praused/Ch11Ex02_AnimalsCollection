a8cb9d893452c4570964bac46d55fbe4349126ba
David Prause <praused@hotmail.com>
2017-05-30 16:38:01 -0400

    completed exercise

---
+++ Ch11Ex02_AnimalsCollection/Animals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11Ex02_AnimalsCollection
+{
+    public class Animals : CollectionBase
+    {
+        public void Add(Animal newAnimal)
+        {
+            List.Add(newAnimal);
+        }
+
+        public void Remove(Animal oldAnimal)
+        {
+            List.Remove(oldAnimal);
+        }
+
+        public Animals()
+        {
+        }
+
+        public Animal this[int animalIndex]
+        {
+            get
+            {
+                return (Animal)List[animalIndex];
+            }
+            set
+            {
+                List[animalIndex] = value;
+            }
+        }
+    }
+}
